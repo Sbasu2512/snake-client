9fa31f6696e2fc2c7778d66517023695a870130a
Sayantan Basu <sayantanbasu@live.in>
2021-06-02 19:28:22 -0400

    final touches

--- input.js
+++ input.js
@@ -12,16 +12,16 @@ const handleUserInput = function(key) {
   connect.write("Name: SB");
   if (key === '\u0003') {
     process.exit();
-  }else if(key === '\u0077'){
+  }else if(key === '\u0077' || key === '\u2191'){
     connect.write("Move: up");
     console.log("W");
-  }else if(key === '\u0073'){
+  }else if(key === '\u0073' || key === '\u2193'){
     connect.write("Move: down");
     console.log("S")
-  }else if(key === '\u0061'){
+  }else if(key === '\u0061' || key === '\u2190'){
     connect.write("Move: left");
     console.log("A")
-  }else if(key === '\u0064'){
+  }else if(key === '\u0064' || key === '\u2192'){
     connect.write("Move: right");
     console.log("D")
   }
