d24b06fcb200765e0e6058cc1542dc24196178a9
Sayantan Basu <sayantanbasu@live.in>
2021-06-02 16:08:42 -0400

    late commit

--- play.js
+++ play.js
@@ -3,10 +3,20 @@ const web = require('./client');
 const connect = web();  
 
 connect.on('connect',()=>{
-  connect.write("Name: SB");
+  connect.write("SB");
+})
+connect.on('data',(c)=>{
+  c.write("move: up");
 })
 
-
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
 
 
 
