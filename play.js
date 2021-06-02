6d78be909a26b713ebcee7df8508ab2a16871e15
Sayantan Basu <sayantanbasu@live.in>
2021-06-02 19:14:56 -0400

    more fn added

--- play.js
+++ play.js
@@ -1,9 +1,5 @@
-const web = require('./client');
-const setupInput = require('./input');
-const connect = web();  
-const input = setupInput();
-
-// setup interface to handle user input from stdin
+const {web} = require('./client');
+const {setupInput} = require('./input');
 
 
 
