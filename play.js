4629d9a38a30adf16d1fc54a8d90ea7da7a62cc9
Sayantan Basu <sayantanbasu@live.in>
2021-06-02 13:10:46 -0400

    final added

--- play.js
+++ play.js
@@ -1,1 +1,13 @@
 const net = require("net");
+
+const web = function () {
+  net.createConnection(
+    {
+      host:'135.23.222.131',
+      port: 50542
+    });
+  
+    web.setEncoding("utf-8");
+
+    return web;
+}
