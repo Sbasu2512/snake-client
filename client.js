e43e9f6d8366246d0127552e195992a250aa482b
Sayantan Basu <sayantanbasu@live.in>
2021-06-02 14:11:52 -0400

    imported codefrom play.js

---
+++ client.js
@@ -0,0 +1,18 @@
+const web = function () {
+  const connect = net.createConnection(
+    {
+      host:'135.23.222.131',
+      port: 50542
+    },()=>{
+      console.log("connection created....");
+    });
+  
+    connect.setEncoding("utf-8");
+//event handler. event name: data. socket methods will refer to client side
+    connect.on('data', (data) => {
+      console.log('Server says: ', data);
+    });
+    
+}
+
+module.exports = connect ;
